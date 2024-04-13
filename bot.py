c655f8cb941910b3bc90632ebc379e048cb27271
Arata-Tomoto <63094716+Arata-Tomoto@users.noreply.github.com>
2024-04-13 19:11:53 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,7 +36,7 @@ async def execute_function():  # 大会を一定時間ごとに調べる関数
 
 
 async def jcg(month, day):  # 情報を取得し，チャンネルに送信する関数
-    ctx = bot.get_channel(os.environ.get("CHANNEL"))
+    ctx = bot.get_channel(CHANNEL_NUM)) #メッセージ送信チャンネルID
     try:
         deck_urls = first.main(month, day)  # デッキのダウンロード，統計の処理
         if (deck_urls == "no_game"):  # グループ予選が見つからなかったら終了
